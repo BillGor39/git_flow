ffc5abdbfbc347e4e16230baf9cdf6361a314dd8
BillGor39 <edwardxie39@hotmail.com>
2024-07-31 20:19:29 +1200

    remove whatup function

--- hello.py
+++ hello.py
@@ -4,14 +4,9 @@ def hello(name):
 def howareyou():
     print("How are you?")
 
-def whatup():
-    print("What's up!")
-
 def main():
     hello("Jack")
     howareyou()
-    whatup()
-
 
 if __name__ == "__main__":
     main()
