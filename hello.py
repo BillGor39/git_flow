79adb94cce9022d228ce114cc0d293020f5c16e7
BillGor39 <edwardxie39@hotmail.com>
2024-07-31 19:46:27 +1200

    add how are you function

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 def hello(name):
     print(f"Hello {name}!")
 
+def howareyou():
+    print("How are you?")
+
 def main():
     hello("Jack")
+    howareyou()
 
 if __name__ == "__main__":
     main()
