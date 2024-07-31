f97a22f8802bfac8f2d3f9ce59dbf4cbc42ae61c
BillGor39 <152768577+BillGor39@users.noreply.github.com>
2024-07-31 20:06:20 +1200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
 def hello(name):
     print(f"Hello {name}!")
 
-def howareyou():
-    print("How are you?")
-
 def main():
     hello("Jack")
-    howareyou()
 
 if __name__ == "__main__":
     main()
